8816741d742606862a0064a897583868d07dbeb6
Tim Kainz <tikaiz@gmx.at>
2026-01-26 00:18:30 +0100

    Change Package Script Mod Location

--- package.sh
+++ package.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 mvn package
-mv ./target/EndermanPlugin-0.1.0.jar /home/tikaiz/.var/app/com.hypixel.HytaleLauncher/data/Hytale/UserData/Mods/
+mv ./target/EndermanPlugin-0.1.0.jar /home/tikaiz/.local/share/Hytale/UserData/Mods/
